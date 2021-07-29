1d899e863f17190af31983b0c6389649de4e1b8d
najihun <51940925+najihun@users.noreply.github.com>
2021-07-30 08:48:05 +0900

    Update Outputs.tf

--- MetalLB/Outputs.tf
+++ MetalLB/Outputs.tf
@@ -1,3 +1,5 @@
+## Metal LB Module 에서 생성된 namespace 'metallb-system' 정보를 Module 밖에서 호출 하기 위해 output 생성
+
 output "metallb_namespace" {
   value = kubernetes_namespace.metallb-system.metadata.0.name
 }
