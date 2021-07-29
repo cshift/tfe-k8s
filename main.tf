25de78dd968b4710e114937ca9a9ed3303f5a487
najihun <51940925+najihun@users.noreply.github.com>
2021-07-30 08:51:57 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-#call Module MetalLB
+# MetalLB Module 호출
 module "MetalLB" {
     source = "./MetalLB"
     lb_ipaddr = "192.168.56.247-192.168.56.249"
 }
 
-#Deploying nginx
+# NGINX Deployment
 resource "kubernetes_deployment" "nginx" {
   metadata {
     name = "nginx-deployment"
@@ -42,7 +42,7 @@ resource "kubernetes_deployment" "nginx" {
   }
 }
 
-# # expose nginx as LB
+## Deploy 한 NGINX 를 'Load Balancer' type의 Service 로 Expose
 resource "kubernetes_service" "nginx_as_lb" {
     metadata {
       name = "nginx-service"
@@ -54,6 +54,7 @@ resource "kubernetes_service" "nginx_as_lb" {
 
     spec {
         selector = {
+            ## 위에서 생성한 NGINX 지정
             app = kubernetes_deployment.nginx.metadata.0.labels.app
         }
 
