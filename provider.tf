306cab922dd8b4eddfbb1dff19f15fa53dee4ed0
najihun <51940925+najihun@users.noreply.github.com>
2021-07-30 08:55:00 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,15 @@
 terraform {
+  ## TFE 환경 관련 정보 입력
   backend "remote" {
-    hostname = "tfe.cshift.co"
-    organization = "cloudshift"
+    hostname = "<TFE HOST 주소>"
+    organization = "<organization 명>"
 
     workspaces {
-      name = "test-tfe-k8s"
+      name = "<workspace 명>"
     }
   }
-
+  
+  ## 사용할 Provider (kubernetes) 와 version 정보
   required_providers {
     kubernetes = {
       source = "hashicorp/kubernetes"
@@ -16,6 +18,7 @@ terraform {
   }
 }
 
+## kubernetes provider 정의 - k8s master node 에 저장된 config 정보 복사하여 Terraform 설치된 local machine 에 저장 후 위치 지정 
 provider "kubernetes" {
     config_path = "~/.kube/config"
 }
